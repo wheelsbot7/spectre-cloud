789c3f1a1900fd26fb32d224197810456faefb75
wheelsbot7 <o.wertzberger@gmail.com>
2025-05-09 16:55:49 -0400

    try github fix

--- astro.config.ts
+++ astro.config.ts
@@ -82,4 +82,13 @@ export default defineConfig({
     include: ["src/components/**.vue"]
   }), alpinejs()],
   adapter: cloudflare(),
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
